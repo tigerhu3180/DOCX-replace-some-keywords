ac2dc2cc56be89bbc38008c64dddea1e323be5ca
tigerhu3180 <573964741@qq.com>
2018-12-21 17:48:05 +0800

    Add files via upload
    The keyword replacement is now available for .doc/.docx doc.
    
    keyword: see DICT ({old word: new word wanted}) in py file.

--- learn.py
+++ learn.py
@@ -1,4 +1,5 @@
 from docx import Document
+from win32com import client
 import os
 
 OLDPATH = "C:\\Users\\qq\\Desktop\\doc\\old"
@@ -18,13 +19,27 @@ DICT = {
   "刘改琴": "冯鑫",
   "2016":"2018",
   "2015":"2017",
+  "TyCloud云管理平台开发项目（三期）":"MIMS通信机房维护巡检管理系统项目（一期）",
+  "河北天翼科贸发展":"内蒙古电力集团蒙电信息通信产业",
+  "有限公司":"有限责任公司",
   "TyCloud":"MIMS"
 }
 
 def main():
     for fileName in os.listdir(OLDPATH):
-        oldFile = OLDPATH + "\\" + fileName
-        newFile = PATH + "\\" + fileName
+        if '.doc' in fileName:
+            word = client.Dispatch('Word.Application')
+            print(OLDPATH + "\\" + fileName)
+            doc = word.Documents.Open(OLDPATH + "\\" + fileName)
+            fileName = fileName.replace('.doc', '.docx') 
+            doc.SaveAs(PATH + "\\" + fileName, 12)  
+            doc.Close()
+            word.Quit()
+
+            oldFile = newFile = PATH + "\\" + fileName
+        else:
+            oldFile = OLDPATH + "\\" + fileName
+            newFile = PATH + "\\" + fileName
 
         document = Document(oldFile)
         document = check(document)
@@ -40,9 +55,9 @@ def check(document):
                         table.cell(row ,col).text = table.cell(row ,col).text.replace(key, value)
 
     # paragraphs
-    for para in document.paragraphs:
+    for para in document.paragraphs:       
         for i in range(len(para.runs)):
-            for key, value in DICT.items():
+            for key, value in DICT.items():                
                 if key in para.runs[i].text:
                     para.runs[i].text = para.runs[i].text.replace(key, value)
 
